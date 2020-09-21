b9fb732b1dce5e346807650702ca21d9ce4e53d6
Nadavsrb <nadavsrb@gmail.com>
2020-09-21 23:41:08 +0300

    Removing unnecessary methods

--- src/server/ClientHandler.hpp
+++ src/server/ClientHandler.hpp
@@ -10,10 +10,6 @@
 namespace server_side {
     class ClientHandler { //needs rule of 5 because smart pointer?????
         public:
-            ClientHandler(const ClientHandler& ClientHandler);
-
-            ClientHandler& operator=(const ClientHandler& source);
-
             virtual void handleClient(const std::unique_ptr<SocketIStream> in,
                  const std::unique_ptr<SocketOStream> out) const = 0;
 
