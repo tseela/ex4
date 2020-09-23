040234b25e2df655efbe28f0776399445717e307
tseela <tseelagamzo@gmail.com>
2020-09-23 16:07:50 +0300

    Changed main.cpp for testing.

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@ using namespace std;
 using namespace matrix;
 using namespace graph;
 using namespace solver;
+using namespace solver::solution;
 
 int main() {
     auto matrix = new MatrixClass(2, 2);
-    matrix->setValue(0,1, 7);
+    matrix->setValue(0, 1, 7);
     matrix->setValue(1, 0, 3);
+    matrix->setValue(1, 1, 3);
+    matrix->setValue(0, 0, 1);
 
     auto p = new problem::MatrixGraphProblem(matrix, 0, 0, 1, 1);
     auto s = new DFS();
