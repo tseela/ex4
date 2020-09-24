516059c9efc016d1b5a928d722300976178f4990
tseela <tseelagamzo@gmail.com>
2020-09-24 18:51:20 +0300

    Working on the experiment.

--- src/main.cpp
+++ src/main.cpp
@@ -12,32 +12,43 @@ using namespace graph;
 using namespace solver;
 using namespace solver::solution;
 
+void initializeMatrix(matrix::MatrixClass* matrix) {
+    for (auto i = 0; i < matrix->getHeight(); ++i) {
+        for (auto j = 0; j < matrix->getWidth(); ++j) {
+            matrix->setValue(i, j, (rand() % 999) + 1);
+        }
+    }
+}
+
+
 int main() {
-    auto matrix = new MatrixClass(3, 2);
-    matrix->setValue(0, 1, 7);
-    matrix->setValue(1, 0, 0);
-    matrix->setValue(1, 1, 3);
-    matrix->setValue(0, 0, 1);
+    string s = "";
+    for (auto i = 0; i < 500; ++i) {
+        auto x = (rand() % 96) + 5;
+        auto y = (rand() % 96) + 5;
 
-    matrix->setValue(2, 1, 5);
-    matrix->setValue(2, 0, 1);
+        auto matrix = new matrix::MatrixClass(x, y);
+        initializeMatrix(matrix);
 
-    auto p = new problem::MatrixGraphProblem(matrix, 0, 0, 1, 1);
-    auto dfs = new graph_solution::DFS();
-    auto bfs = new graph_solution::BFS();
-    auto a_star = new graph_solution::AStar();
-    auto best = new graph_solution::BestFS();
+        s += to_string(x) + "," + to_string(y) + "\n" + matrix->toString() + "\n\n";
 
-    auto solver1 = new Solver(p, dfs);
-    auto solver2 = new Solver(p, bfs);
-    auto solver3 = new Solver(p, a_star);
-    auto solver4 = new Solver(p, best);
-    try {
-        cout << solver1->getStringSolution(false) << endl;
-        cout << solver2->getStringSolution(false) << endl;
-        cout << solver3->getStringSolution(false) << endl;
-        cout << solver4->getStringSolution(false) << endl;
-    } catch (matrix::ErrorCodeException e) {
-        e.printErrorMessage();
+        auto p = new problem::MatrixGraphProblem(matrix, 0, 0, matrix->getHeight() - 1, matrix->getWidth() - 1);
+        auto dfs = new graph_solution::DFS();
+        auto bfs = new graph_solution::BFS();
+        auto a_star = new graph_solution::AStar();
+        auto best = new graph_solution::BestFS();
+
+        auto solver1 = new Solver(p, dfs);
+        auto solver2 = new Solver(p, bfs);
+        auto solver3 = new Solver(p, a_star);
+        auto solver4 = new Solver(p, best);
+        try {
+            cout << solver1->getStringSolution(false) << endl;
+            cout << solver2->getStringSolution(false) << endl;
+            cout << solver3->getStringSolution(false) << endl;
+            cout << solver4->getStringSolution(false) << endl;
+        } catch (matrix::ErrorCodeException e) {
+            e.printErrorMessage();
+        }
     }
 }
