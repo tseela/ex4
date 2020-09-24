b1cb24cef1a512773922a593d91ff9c863c4d6af
tseela <tseelagamzo@gmail.com>
2020-09-24 14:17:20 +0300

    Added some notes.

--- src/solver/cache_manager/Operation.cpp
+++ src/solver/cache_manager/Operation.cpp
@@ -8,7 +8,9 @@ solver::cache::Operation::Operation(const Problem* problem, const Solution* solu
 }
 
 void solver::cache::Operation::writeToFile(const std::string& fileName) const { m_solution->writeToFile(m_problem, fileName); }
+
 string solver::cache::Operation::getOutputFileType() const { return m_solution->getOutputFileType(); }
+
 string solver::cache::Operation::getCacheString() const {
     return m_solution->getCacheCode() + " " + m_solution->getCacheString() + " " + m_problem->getCacheString();
 }
